8a263bdc10e28a055e9f4f0c1fa517d6c8832b6c
Kazuya Saito <saito@smartalgorithm.co.jp>
2019-10-22 14:09:49 +0900

    Upload and attach an image to a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:body, :image)
     end
 end
